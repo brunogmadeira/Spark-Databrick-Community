9e0e488bd74281fad76e79743f61b7aea3dadfab
Bruno Girardi Madeira <girardi791@gmail.com>
2025-05-04 21:53:00 -0300

    Adicionando configuracao adls azure

---
+++ adls-azure/variables.tf
@@ -0,0 +1,8 @@
+# Lista de variaveis utilizadas nos arquivos de terraform
+variable "resource_group_name" {
+  default = "learn-68d09660-8f4f-4af0-9472-e611d5ea9764"
+}
+
+variable "location" {
+  default = "westus"
+}
